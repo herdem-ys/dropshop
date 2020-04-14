fcfdfd5d94d080946767c40a5d2bcf6df0fa9221
Herdem Can Yasit <eddsworld@hotmail.de>
2020-04-14 16:42:52 +0200

    added comments and search bar improved

--- searchbar.js
+++ searchbar.js
@@ -1,9 +1,7 @@
-
-
 function search(){
-      var value = $("#searchInput").val().toLowerCase();
-      $("#myTable tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      var value = $("#searchInput").val().toLowerCase(); // Der Wert im searchInput wird zu lowercase konvertiert
+      $("#myTable tr").filter(function() { 
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1); // hier
       });
 }
 
